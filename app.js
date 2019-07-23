299b19a6e52c3741f11d7ba89e026980cd39b0c5
Karl Wenninger <karl.wenninger@gmail.com>
2019-07-22 22:40:34 -0400

    New and improved clean code version!

--- app.js
+++ app.js
@@ -1,61 +1,65 @@
 const display = document.querySelector('#display');
 const keysContainer = document.querySelector('#keys-container');
 
-let val1 = 0,
-    val2 = 0,
-    operator = '',
+// get key click, run function
+keysContainer.addEventListener('click', (e) => {
+    const keyClassList = e.target.classList;
+    const keyValue = e.target.innerText;
+
+    if (keyClassList.contains('num-key')) { return numKey(keyValue); }
+    if (keyClassList.contains('op-key')) { return opKey(keyValue); }
+    if (keyClassList.contains('calc-key')) { return calcKey(); }
+    if (keyClassList.contains('deci-key')) { return deciKey(); }
+    if (keyClassList.contains('clear-key')) { return clearKey(); }
+})
+
+let val1 = null,
+    val2 = null,
+    operator = null,
     decimal = false;
 
-keysContainer.addEventListener('click', (e) => {
-    const keyPress = e.target;
-
-    if (keyPress.classList.contains('num-key')) {
-        if (display.innerText == 0) {
-            display.innerText = keyPress.innerText;
-        } else if (val1 != 0 && val2 == 0) {
-            display.innerText = keyPress.innerText;
-            val2 = display.innerText;
-        } else {
-            display.innerText += keyPress.innerText;
-        }
+function numKey(keyValue) {
+    // 0 == on load default value
+    if (display.innerText == 0 && decimal == false) {
+        display.innerText = keyValue;
     }
+    // 2nd opKey click runs calcKey()
+    else if (operator != null && val2 == null) {
+        display.innerText = keyValue;
+        val2 = 'temp';
+    } else {
+        display.innerText += keyValue;
+    };
+}
 
-    if (keyPress.className == 'op-key') {
-        val1 = display.innerText;
-        if (val1 == 0) {
-            return
-        } else {
-            if (keyPress.innerText == '+') { operator = 'add'; }
-            if (keyPress.innerText == '-') { operator = 'subtract'; }
-            if (keyPress.innerText == '*') { operator = 'multiply'; }
-            if (keyPress.innerText == '÷') { operator = 'divide'; }
-        }
-        console.log(`op ${val1} ${val2} ${operator} ${display.innerText}`)
-        return val1, operator, decimal = false;
-    }
+function opKey(keyValue) {
+    // 2nd click on opKey
+    if (operator != null) { calcKey(); }
 
-    if (keyPress.className == 'calc-key') {
-        val2 = display.innerText;
+    if (keyValue == '+') { operator = 'add'; }
+    if (keyValue == '-') { operator = 'subtract'; }
+    if (keyValue == '*') { operator = 'multiply'; }
+    if (keyValue == '÷') { operator = 'divide'; }
 
-        if (operator == 'add') { display.innerText = parseFloat(val1) + parseFloat(val2); }
-        if (operator == 'subtract') { display.innerText = parseFloat(val1) - parseFloat(val2); }
-        if (operator == 'multiply') { display.innerText = parseFloat(val1) * parseFloat(val2); }
-        if (operator == 'divide') { display.innerText = parseFloat(val1) / parseFloat(val2); }
-        console.log(`calc ${val1} ${val2} ${operator} ${display.innerText}`)
+    val1 = display.innerText;
+    return val1, operator;
+}
 
-        return val1 = display.innerText, val2 = 0, decimal = false;
-    }
+function calcKey() {
+    val2 = display.innerText;
 
-    if (keyPress.className == 'deci-key') {
-        if (decimal == true) {
-            return
-        } else {
-            display.innerText += keyPress.innerText;
-            return decimal = true;
-        }
-    }
+    if (operator == 'add') { display.innerText = parseFloat(val1) + parseFloat(val2); }
+    if (operator == 'subtract') { display.innerText = parseFloat(val1) - parseFloat(val2); }
+    if (operator == 'multiply') { display.innerText = parseFloat(val1) * parseFloat(val2); }
+    if (operator == 'divide') { display.innerText = parseFloat(val1) / parseFloat(val2); }
+console.log(`${val1} ${val2}`)
+    return val1 = display.innerText, val2 = null;
+}
 
-    if (keyPress.className == 'clear-key') {
-        return display.innerText = 0, val1 = 0, val2 = 0, operator = '', decimal = false;
-    }
-})
+function deciKey() {
+    if (decimal == false) { return display.innerText += '.', decimal = true }
+}
+
+function clearKey() {
+    return display.innerText = 0, val1 = null, val2 = null, operator = null, decimal = false;
+}
