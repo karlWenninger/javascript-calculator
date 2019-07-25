b9f8091f359ce5d74dc96618e1ba6795f284692c
Karl Wenninger <karl.wenninger@gmail.com>
2019-07-25 08:30:01 -0400

    Added deciPlace(), formatOutput(), minor styling tweaks

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const display = document.querySelector('#display');
 const keysContainer = document.querySelector('#keys-container');
 
-let
-    inpNum = null,
+let inpNum = null,
+    result = null,
     val1 = null,
     val2 = null,
     operator = null,
@@ -19,9 +19,10 @@ keysContainer.addEventListener('click', (e) => {
     if (keyClassList.contains('calc-key')) return currKey = 'calcKey', calc();
     if (keyClassList.contains('deci-key')) return deci();
     if (keyClassList.contains('clear-key')) return clear();
-})
+});
 
 function num(keyValue) {
+    inpNum = display.innerText;
     // clear display 0 on page load
     if (display.innerText === '0') {
         inpNum = keyValue;
@@ -32,9 +33,9 @@ function num(keyValue) {
         val2 = 'temp';
     } else {
         inpNum += keyValue;
-    };
+    }
 
-    return display.innerText = formatDisplayFont(inpNum);
+    return display.innerText = formatOutput(inpNum);
 }
 
 function op(keyValue) {
@@ -53,14 +54,17 @@ function op(keyValue) {
 function calc() {
     val2 = display.innerText;
 
-    let result = null;
+    result = null;
 
     if (operator == 'add') { result = parseFloat(val1) + parseFloat(val2); }
     if (operator == 'subtract') { result = parseFloat(val1) - parseFloat(val2); }
     if (operator == 'multiply') { result = parseFloat(val1) * parseFloat(val2); }
     if (operator == 'divide') { result = parseFloat(val1) / parseFloat(val2); }
-    // console.log(result.toString().length)
-    return display.innerText = formatDisplayFont(result), val2 = null, prevKey = currKey;
+
+    deciPlaces(result);
+    formatOutput(result);
+
+    return display.innerText = result, val2 = null, prevKey = currKey;
 }
 
 function deci() {
@@ -72,16 +76,21 @@ function clear() {
     return display.innerText = 0, val1 = null, val2 = null, operator = null, currKey = null, prevKey = null, display.style.fontSize = '3em';
 }
 
+function deciPlaces(number) {
+    if (number.toString().split('.')[1] > 4) {
+        return result = parseFloat(number.toFixed(4));
+    } else { return number; }
+}
 
-function formatDisplayFont(number) {
-    let numLength = number.toString().length;
+function formatOutput(result) {
+    let numLength = result.toString().length;
 
-    if (numLength > 8 && numLength <= 10) {
-        display.style.fontSize = '2.3em';
-    } else if (numLength > 10 && numLength <= 12) {
+    if (numLength > 9 && numLength <= 11) {
+        display.style.fontSize = '2.5em';
+    } else if (numLength > 11 && numLength <= 13) {
         display.style.fontSize = '2em';
-    } else if (numLength > 12) {
-        return 'too big!!! (clear)';
+    } else if (numLength > 14) {
+        return display.style.fontSize = '2em', 'too big!!! (clear)';
     }
-    return number;
+    return result;
 }
